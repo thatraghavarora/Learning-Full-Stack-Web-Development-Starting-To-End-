e224ec0a490f2d8fbfce714fcdb5eac0a38d9581
thatraghavarora <raghavarora930@gmail.com>
2025-05-29 22:41:23 +0530

    UPDATE

---
+++ Interview Question Js/answer.js
@@ -0,0 +1,21 @@
+//  console.log("1"-1);// 1
+//    setTimeout(function() {console.log(40)}, 10);
+//    console.log(22) // 2
+//    setTimeout(function() {console.log(91)}, 9);
+//    setTimeout(function() {console.log(23)}, 10);
+
+// let x = {} , y = {name:"RAJ"} , z = {name:"RAGHAV"}
+// x[y]={name:"Salman"}
+// x[z] = {name:"Sal"}
+
+// console.log(x[y]);
+
+// console.log(NaN === NaN);
+
+// console.log(null == undefined); 
+
+// console.log(1>2<3); 
+
+for(let i=0; i<3; i++){
+    setTimeout(function() {console.log(i)}, 100);
+}
